ce03a372f09ff077e21a2943f4362743057b5afe
Kyouken <yogapan85321@icloud.com>
2016-12-15 03:09:08 +0800

    Server Hello World

---
+++ index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.body = { message: err.message };
+    ctx.status = err.status || 500;
+  }
+});
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
