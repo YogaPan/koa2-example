9ef2beafde3402fa06e8027bc3b25a590aea4209
Kyouken <yogapan85321@icloud.com>
2016-12-18 02:10:34 +0800

    Add get members request test code.

--- test.js
+++ test.js
@@ -1,6 +1,34 @@
 import test from 'ava';
 import request from 'request';
 
-test('index', async t => {
+test('index page', async t => {
+  t.plan(1);
+
+  const { res, body } = await getRequest('http://127.0.0.1:3000');
+  t.is(res.statusCode, 200);
+});
+
+test('get members', async t => {
+  t.plan(1);
+
+  const { res, body } = await getRequest('http://127.0.0.1:3000/api/members');
+  t.is(res.statusCode, 200);
+});
+
+test('post members', async t => {
   // TODO
 });
+
+function getRequest(url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error)
+        resolve({
+          res,
+          body,
+        });
+      else
+        reject(error);
+    });
+  });
+}
