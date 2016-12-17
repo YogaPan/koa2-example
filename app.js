229c575762f26b69d913ba49ad1ca0842eb6439f
Kyouken <yogapan85321@icloud.com>
2016-12-17 16:53:00 +0800

    Add server logger and static pages.
    Change restful service from '/' to '/api'

--- app.js
+++ app.js
@@ -2,25 +2,71 @@ const Koa = require('koa');
 const app = new Koa();
 
 const router = require('koa-router')();
+const logger = require('koa-logger');
+const serve = require('koa-static');
+
 const conn = require('./models/index'); // MySQL connection.
 const port = 3000; // Set server listen port.
 
+app.use(logger());
+app.use(serve(__dirname + '/public'));
+
 // Restful router.
 router
-  .get('/', async ctx => {
+  .get('/api/members', async ctx => {
     ctx.body = await conn.query('SELECT * FROM Members');
   })
-  .get('/fuck', async ctx => {
-    ctx.body = 'Fuck';
-  })
+  .post('api/members', async ctx => {
+    // TODO
+  });
 
-// Error handling middleware.
+// Catch all unhandled server internal errors and display message.
 app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
     ctx.body = { message: err.message };
     ctx.status = err.status || 500;
+
+    // Show error message to server side.
+    console.log(err);
+
+    // Show error message to client side.
+    switch (ctx.accepts('html', 'json')) {
+      case 'html':
+        ctx.type = 'html';
+        ctx.body = `<p>${err.message}</p>`;
+        break;
+      case 'json':
+        ctx.type = 'json';
+        ctx.body = { message: err.message  };
+        break;
+      default:
+        ctx.type = 'text';
+        ctx.body = err.message;
+    }
+  }
+});
+
+// 404 Not Found Page.
+app.use(async (ctx, next) => {
+  await next();
+  if (ctx.status !== 404)
+    return;
+  ctx.status = 404;
+
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>404 Page Not Found</p>'
+      break;
+    case 'json':
+      ctx.type = 'json';
+      ctx.body = { message: '404 Page Not Found' };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = '404 Page Not Found';
   }
 });
 
